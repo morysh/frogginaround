32968bb67508fb36b159efb31addc1a75215ec4c
Morysh <anon@lavache.com>
2022-04-10 19:11:56 -0700

    Styles for headings and link in pages/articles

--- wordpress/data/wp-content/themes/frogginaround/header.php
+++ wordpress/data/wp-content/themes/frogginaround/header.php
@@ -21,7 +21,7 @@ require_once('helpers/menus-helper.php')
 	<base href="/wp-content/themes/frogginaround/angular/"/>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital@0;1&family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 	<script>
 		const ORIGIN = window.location.origin;
 		const GLOBALS = {
